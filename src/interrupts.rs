2b64ea052c39b2d43631dd4bbc1dce2ad0fc2b2d
Chris Thurber <skiman6010@gmail.com>
2019-06-30 22:53:57 -0400

    Added basic paging calls to kernel.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -44,8 +44,23 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
+}
+
+use x86_64::structures::idt::PageFaultErrorCode;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    _error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 pub fn init_idt() {
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
+    let ptr = 0xdeadbeaf as *mut u32;
+    unsafe {*ptr = 42; }
+
     #[cfg(test)]
     test_main();
 
